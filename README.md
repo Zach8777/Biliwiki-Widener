2380ad95d7a2e4b01ed9ddf78300cf6b4fd8fbaf
Zach8777 <zach8777@163.com>
2024-04-17 19:54:16 -0700

    Add tampermonkey script

---
+++ README.md
@@ -0,0 +1,7 @@
+## Biliwiki Widener / Bilibili wiki 扩宽
+Tampermonkey 脚本
+
+扩宽哔哩哔哩wiki的主体部分，避免在高分辨率、宽屏上流出过多空白。
+
+不确保与其它部件的兼容性，可能出现元素重叠。
+
---
+++ biliwiki-widener.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         bilibili wiki扩宽
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Widen the main component of Bilibili Wiki for high resolution screens.
+// @author       Zach8777
+// @match        https://wiki.biligame.com/*
+// @icon         https://static.hdslb.com/images/favicon.ico
+// @grant        GM_addStyle
+// @grant        GM_log
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    GM_addStyle(".game-bg.container { width: 90%}");
+    GM_addStyle(".mw-body.container { width: 90%}");
+    GM_addStyle(".bui-sns-info { margin-left: 90%!important;;}")
+
+})();
